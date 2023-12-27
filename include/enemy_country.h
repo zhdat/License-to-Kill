08198273ba41d96b650d6a2c5a8794cbb822eaa6
Calliste Linux <calliste.ravix@ecole.ensicaen.fr>
2023-12-27 16:56:48 +0100

    Debug MailBox plus Timer youhouuuuu

--- include/enemy_country.h
+++ include/enemy_country.h
@@ -1,7 +1,8 @@
 #ifndef TP_SYS2_ENEMY_COUNTRY_H
 #define TP_SYS2_ENEMY_COUNTRY_H
 
-# define DECALAGE 3
+#define DECALAGE 3
+
 #include "common.h"
 
 struct message_s {
@@ -12,8 +13,10 @@ struct message_s {
 
 typedef struct message_s message_t;
 
-void writeToSharedMemory(char *message, int valid_message);
-void decrpyt_message(char *message);
-int isValidMessage(char *message);
+void writeToSharedMemory(char* message, int valid_message);
+
+void decrpyt_message(char* message);
+
+int isValidMessage(char* message);
 
 #endif //TP_SYS2_ENEMY_COUNTRY_H
