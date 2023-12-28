e8390526c4e85ee6075a40cd72b4fd65078a9f93
Calliste Ravix <calliste.ravix@ecole.ensicaen.fr>
2023-12-28 22:21:19 +0100

    Envoie signal SIGUSR1 vers espions, print pid

--- src/character/counterintelligence_officer.c
+++ src/character/counterintelligence_officer.c
@@ -51,7 +51,9 @@ void move_counter_intelligence_officer(officer_thread_args_t* arg, int row, int 
                 log_info("Counter-intelligence officer %d has caught a spy at %d:%d", officer->character.id,
                          officer->character.row, officer->character.column);
                 log_info("The pid of the spy is %d", agent->character.pid);
-                //kill(agent->character.pid, SIGUSR1);
+                if (agent->character.pid != 0){
+                    kill(agent->character.pid, SIGUSR1);
+                }
                 break; // Supposons qu'il ne peut y avoir qu'un seul espion par cellule
             }
         }
